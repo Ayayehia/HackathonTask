355eb41c7fdd82b86ac44cd038d4c01c9f94741f
ayayehia <ayanader8@gmail.com>
2024-02-01 16:43:07 +0200

    style:Changes

--- src/components/TrackCard.tsx
+++ src/components/TrackCard.tsx
@@ -14,19 +14,19 @@ const TrackCard: React.FC<TrackCardProps> = ({
   console.log(number, "number passed as prop");
   return (
     <div
-      className="flex flex-col h-[197px] w-full justify-center items-start  rounded-15 px-8 rounded-2xl mt-12"
+      className="flex flex-col justify-center items-start p-8 pb-10  rounded-2xl mt-12"
       style={{
         background:
           "linear-gradient(253deg, rgba(255, 122, 122, 0.56) 14.23%, #2A0FD3 101.22%)",
       }}
     >
-      <div className="flex justify-between w-full text-2xl font-medium tracking-wider">
+      <div className="flex justify-between w-full text-2xl  tracking-wider mb-2">
         <h3>{title}</h3>
         <h4> {number} members</h4>
       </div>
       <div className="flex flex-col ">
-        <h4 className=" text-4xl leading-normal font-bold">{subTitle}</h4>
-        <p className=" text-xl  mb-[18px]">{code}</p>
+        <h4 className=" text-4xl  font-bold mb-5">{subTitle}</h4>
+        <p className=" text-xl ">{code}</p>
       </div>
     </div>
   );
